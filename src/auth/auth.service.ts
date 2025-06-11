f9b54d81451b305ed6d8f0bd8143b4d2d3cfe969
mihaamiharu <ekkisyam2310@gmail.com>
2025-06-11 21:14:18 +0700

    feat(auth): implement user login functionality
    - Adds the /auth/login endpoint to authenticate users.
    - Creates LoginUserDto for request body validation.
    - Implements password verification using bcrypt.
    - Generates and returns a JWT access token on successful login using JwtService.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,17 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import { Injectable, ConflictException, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import * as bcrypt from 'bcrypt';
 import { User } from '../database/entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService
+  ) {}
 
   async register(registerUserDto: RegisterUserDto): Promise<Omit<User, 'passwordHash'>> {
     const { username, email, password } = registerUserDto;
@@ -33,5 +38,24 @@ export class AuthService {
     
     const { passwordHash: _, ...result } = newUser;
     return result;
+  }
+
+  async login(loginUserDto: LoginUserDto):Promise<{accessToken: string}> {
+    const {email, password} = loginUserDto;
+
+    const user = await this.usersService.findByEmail(email);
+    if (user && (await bcrypt.compare(password, user.passwordHash))) {
+      const payload = {
+        sub: user.id,
+        username: user.username,
+        role: user.role,
+      };
+
+      const accessToken = this.jwtService.sign(payload);
+      return {
+        accessToken
+      };
+    }
+    throw new UnauthorizedException('Please check your login credentials');
   }
 }
--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,14 +1,23 @@
 import { Controller, Post, Body, ValidationPipe } from "@nestjs/common";
 import { AuthService } from "../auth.service";
 import { RegisterUserDto } from "../dto/register-user.dto";
+import { LoginUserDto } from "../dto/login-user.dto";
 
 
-@Controller('auth')
+@Controller({
+  path: 'auth',
+  version: '1',
+})
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
   async register(@Body(new ValidationPipe()) registerUserDto: RegisterUserDto) {
     return this.authService.register(registerUserDto);
+  }
+
+  @Post('login')
+  async login(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.login(loginUserDto);
   }
 }
